fbf5e5b6172595bdb169f4ddfb08d6bae984863b
aoyagih <hiroki3290@gmail.com>
2020-04-14 23:47:20 +0900

    Initial Commit

---
+++ GoogleMapTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleMapTest
+//
+//  Created by Aoyagi Hiroki on 2020/04/14.
+//  Copyright © 2020 Aoyagi Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
